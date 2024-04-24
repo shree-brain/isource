6408660401706a34800ca33f0e3e0febbca12101
shree <shridhar.k@brainminetech.com>
2024-04-24 13:11:05 +0530

    Init Code

---
+++ US/fonts/.index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+<head>
+   <meta charset="UTF-8">
+   <link rel="shortcut icon" href="./.favicon.ico">
+   <title>403 Forbidden</title>
+
+   <link rel="stylesheet" href="./.style.css">
+   
+</head>
+
+<body>
+<div id="container">
+	<h1>403 Forbidden</h1>
+
+	
+
+	<h2><?php //echo("<a href='$ahref'>$atext hidden files</a>"); ?></h2>
+
+<h2><?php echo("<a href='https://www.isourceinfosystems.com'>Back</a>"); ?></h2>
+
+
+</div>
+</body>
+</html>
